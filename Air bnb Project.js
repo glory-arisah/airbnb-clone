ed767827f76cbf31997f7972d5efa55f811d9dd6
glarisah <gloryarisah@gmail.com>
2020-08-04 16:32:23 +0100

    first commit

---
+++ Air bnb Project.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', () => {
+    homePageList.style.display ="none"
+})
+
+const homePageDropDown = document.querySelector(".icon-header");
+const anew = document.querySelector(".anew")
+const homePageList = document.querySelector(".dropdown")
+
+homePageDropDown.addEventListener('mouseover', () => {
+  homePageList.style.display = "block"  
+})
+
+homePageList.addEventListener('mouseover', () => {
+    homePageList.style.display = "block"
+})
+
+homePageList.addEventListener('mouseout', () => {
+   homePageList.style.display= "none"
+})
